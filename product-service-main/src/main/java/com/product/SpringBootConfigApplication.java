6d3966dddb08be68849440b8129f0c6a2dd4e575
Harshith1234567 <53342028+Harshith1234567@users.noreply.github.com>
2022-06-20 18:58:01 +0530

    Add files via upload

---
+++ product-service-main/src/main/java/com/product/SpringBootConfigApplication.java
@@ -0,0 +1,13 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(scanBasePackages= {"com.product"})
+public class SpringBootConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigApplication.class, args);
+	}
+}
